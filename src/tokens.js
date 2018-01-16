5cd15ddfd1d1b2ea66eb02df036efe4987057a80
Pollie <pieterjan.pollie@gmail.com>
2018-01-16 07:19:06 +0100

    Add files via upload

---
+++ src/tokens.js
@@ -0,0 +1,3 @@
+module.exports.botToken = ''; //input your bot key
+module.exports.youtubeToken = ''; //input Youtube API key
+module.exports.devId = ''; //Used to lockdown, no purpose to use when using another bot
