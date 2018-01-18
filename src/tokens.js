46e5cf5af24a48f185e4869cef0e056533ca6f19
Pollie <12276548+ThePjpollie@users.noreply.github.com>
2018-01-18 05:13:18 +0100

    Add files via upload

--- src/tokens.js
+++ src/tokens.js
@@ -1,3 +1,3 @@
-module.exports.botToken = 'Mzk2MjQ3OTk2NTE5Njc3OTUz.DSeqNw.T-9jZ-6xaqRTrGBSNmnkWfjoFjc'; //input your bot key
-module.exports.youtubeToken = 'AIzaSyBBDkMDKzRSdGLr-Ho6DS5hFDszrsQEk2M'; //input Youtube API key
-module.exports.devId = '149223090134450177'; //Used to lockdown, no purpose to use when using another bot
+module.exports.botToken = ''; //input your bot key
+module.exports.youtubeToken = ''; //input Youtube API key
+module.exports.devId = ''; //Used to lockdown, no purpose to use when using another bot
