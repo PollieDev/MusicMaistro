130f4445b4692e7db4a18c7d1b7deb0146bd3eb5
Pieter Jan Pollie <pieterjanpollie@gmail.com>
2018-01-18 05:21:22 +0100

    .

--- src/tokens.js
+++
@@ -1,3 +0,0 @@
-module.exports.botToken = ''; //input your bot key
-module.exports.youtubeToken = ''; //input Youtube API key
-module.exports.devId = ''; //Used to lockdown, no purpose to use when using another bot
